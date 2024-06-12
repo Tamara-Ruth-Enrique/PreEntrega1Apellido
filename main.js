649efcadcfe2fe2f17382c40cb65d2cfa9bdb739
Tamara-Ruth-Enrique <165954141+Tamara-Ruth-Enrique@users.noreply.github.com>
2024-06-12 15:13:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,23 @@
 
-        // Función para calcular el promedio
-        function calcularPromedio() {
-            let totalNotas = 0;
-            let cantidadNotas = 0;
+        function calcularPromedio(numeros) {
+    const suma = numeros.reduce((acumulador, valorActual) => acumulador + valorActual, 0);
+    return suma / numeros.length;
+}
 
-            // Ciclo para recibir las notas hasta que se ingrese "fin"
-            while (true) {
-                let nota = prompt("Ingrese una nota (o escriba 'fin' para terminar):");
+//objetos que tienen varios arrays de números
+const datos = {
+    matematicas: [8, 9, 6, 6, 7],
+    fisica: [8, 7, 9, 6, 8],
+    quimica: [7, 6, 6, 6, 7]
+};
 
-                // Verificar si se ingresó "fin"
-                if (nota.toLowerCase() === "fin") {
-                    break; // Salir del ciclo
-                }
+// Calculo de promedio de cada materia
+function mostrarPromedios(datos) {
+    for (let materia in datos) {
+        const promedio = calcularPromedio(datos[materia]);
+        console.log(`El promedio de ${materia} es ${promedio.toFixed(2)}`);
+    }
+}
 
-                // Convertir la nota a número
-                nota = parseFloat(nota);
-
-                // Verificar si la nota es válida (osea que no sea un texto )
-                if (!isNaN(nota)) {
-                    totalNotas += nota;
-                    cantidadNotas++;
-                } else {
-                    alert("Por favor, ingrese una nota válida."); 
-                }
-            }
-
-            // Calcular el promedio
-            const promedio = totalNotas / cantidadNotas;
-
-            // Mostrar el promedio en un alert
-            alert(`El promedio de las ${cantidadNotas} notas ingresadas es: ${promedio.toFixed(2)}`);  // se utiliza tofixed para retornar un numero en decimales
-        }
-
-        // Llamar a la función para empezar el cálculo del promedio
-        calcularPromedio();
+// Llamar a la función para mostrar los promedios
+mostrarPromedios(datos);
